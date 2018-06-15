10a434e6e1ff1054993bf95cae56ec42471f57ea
Jason Dale <jdale215@gmail.com>
2018-06-15 18:25:40 -0400

    pushing changes for the weekend

--- assets/app.js
+++ assets/app.js
@@ -1,100 +1,48 @@
 var palindromeObject = {
   input1:[],
-  input2:[]
+  input2:[],
 };
+
+var duplicateObject = {
+  currentWord:'',
+  topWord: '',
+  topLetter: '',
+  topLetterCount: 0
+}
 
 
 function palindromeTest(inputPhrase) {
-  let splitPhrase = splitString(inputPhrase.toLowerCase(), "");
-  console.log(splitPhrase.length);
-  console.log(splitPhrase)
-  console.table(splitPhrase);
-  palindromeObject.input1 = splitPhrase
+  inputPhrase = noSpace(inputPhrase)
+  let splitPhrase = splitString(inputPhrase, "");
+  let splitPhraseReverse = revString(inputPhrase)
+  splitPhrase = lowerIt(splitPhrase)
+  splitPhraseReverse = splitString(splitPhraseReverse, "")
+  splitPhraseReverse = lowerIt(splitPhraseReverse)
   splitPhrase = removeSpecialChars(splitPhrase)
-  //console.log(splitPhrase)
-
-  console.log(palindromeObject)
-  console.log(splitPhrase, "fwd");
-  let splitPhraseReverse = reverseIt(splitPhrase)
-  console.log(splitPhraseReverse, "rev");
+  splitPhraseReverse = removeSpecialChars(splitPhraseReverse)
+  palindromeObject.input1 = splitPhrase
   palindromeObject.input2 = splitPhraseReverse
-  console.log(palindromeObject)
-  var results
-  results = isPalindrome(splitPhrase, splitPhraseReverse)
-  console.log(results);
-}
-
-function splitString(phrase, sep) {
-  let chars = phrase.split(sep)
-  return chars
-}
-
-//somehow this is borking things. unSpecial ends up 4,3,3,4 when using 1234. ???
-
-// function removeSpecialChars(special) {
-//   console.table(special);
-//   let unSpecial = []
-//   console.table(unSpecial)
-//   for (let i = 0; i < special.length; i++) {
-//     console.log(i, "special iteration number")
-//     console.log(special[i])
-//     if (special[i].match(/[a-zA-Z0-9]/gmi)) {
-//       let temp = special[i]
-//       console.table(temp)
-//       console.log(temp)
-//       unSpecial.push(temp)
-//       console.log(special[i])
-//       console.log(unSpecial)
-//       console.table(special)
-//       console.table(unSpecial)
-//     }
-//   }
-//   console.table(unSpecial)
-//   return unSpecial
-// }
-
-function removeSpecialChars(special) {
-
-  for (let i = 0; i < special.length; i++) {
-    console.log(special[i]);
-    if (special[i].match(/[^a-zA-Z0-9]/g)) {
-      console.log("splice");
-      console.log(special.splice(i, 1))
-    }
-}
-  console.log(special);
-  return special
-}
-
-function reverseIt(inputPhrase) {
-  var outputPhrase = []
-  outputPhrase = inputPhrase
-  console.log(inputPhrase, "in")
-  // outputPhrase = outputPhrase.reverse()
-  for (let i = 0; i < outputPhrase.length; i++) {
-    console.log(i, "i")
-    let x = inputPhrase.length-i-1
-    console.log(x, "x")
-    outputPhrase[i] = inputPhrase[x]
-
-    console.log(outputPhrase[i])
-  }
-  return outputPhrase
+  var results = isPalindrome(palindromeObject.input1, palindromeObject.input2)
 }
 
 function isPalindrome(input1, input2) {
-
-  console.log(input1);
-  console.log(input2);
   for (let i = 0; i < input1.length; i++) {
-    console.log(input1[i], input2[i]);
+    console.log(input1[i], input2[i], "compare");
     if (input1[i] != input2[i]) {
-      $("#palindromeResult1").html("Output: False")
       console.log("fake news");
+      $("#palindromeResult1").html("Output: False")
       break
     } else {
       $("#palindromeResult1").html("Output: True")
-      return true
+
     }
   }
+}
+
+function duplicateTest(inputPhrase) {
+  let duplicateSplit = splitString(inputPhrase, " ")
+  for (var i = 0; i < duplicateSplit.length; i++) {
+    let topWord = countLetters(duplicateSplit[i])
+  }
+  console.log(duplicateObject);
 }
---
+++ assets/helper.js
@@ -0,0 +1,84 @@
+function noSpace (input) {
+  let output = input.split(" ").join("")
+  return output
+}
+
+function revString (input) {
+  input = input.split("").reverse().join("")
+  return input
+}
+
+function splitString (phrase, sep) {
+  let chars = phrase.split(sep)
+  return chars
+}
+
+function lowerIt (input) {
+  for (var i = 0; i < input.length; i++) {
+    if (input[i].match(/[a-zA-Z]/gmi)) {
+      input[i] = input[i].toLowerCase()
+    }
+  }
+  return input
+}
+
+
+function removeSpecialChars(special) {
+
+  for (let i = 0; i < special.length; i++) {
+
+    if (special[i].match(/[^a-zA-Z0-9\ ]/g)) {
+      special.splice(i, 1)
+    }
+}
+  console.log(special);
+  return special
+}
+
+function reverseIt(inputPhrase) {
+  var outputPhrase = []
+  outputPhrase = inputPhrase
+  for (let i = 0; i < outputPhrase.length; i++) {
+    console.log(i, "i")
+    let x = inputPhrase.length-i-1
+    console.log(x, "x")
+    outputPhrase[i] = inputPhrase[x]
+
+    console.log(outputPhrase[i])
+  }
+  return outputPhrase
+}
+
+
+// function countLetters (word) {
+//   for (var i = 0; i < word.length; i++) {
+//     if (word[i] == duplicateObject.topLetter) {
+//       duplicateObject.word = word
+//       duplicateObject.topLetterCount++
+//
+//     }
+//   }
+// }
+
+function countLetters (word) {
+  console.log(word);
+  let alphabet = []
+  word = splitString(word, "")
+  console.log(word);
+  for (var i = 0; i < word.length; i++) {
+    console.log(word[i]);
+    let searchLetter = word[i].match(alphabet[0-alphabet.length])
+    console.log(searchLetter);
+    if (searchLetter == true) {
+      console.log("found");
+      duplicateObject.topWord = word
+      duplicateObject.topLetter = word[i]
+      console.log(alphabet);
+    } else {
+      console.log("not found");
+      console.log(alphabet.push(word[i]))
+      console.log(alphabet);
+    }
+  }
+
+}
